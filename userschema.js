a5a0788a963488025111436d7c66740462e20a71
jykinturah <jykinturah@gmail.com>
2019-05-28 11:49:16 -0700

    Add new word count schema

---
+++ userschema.js
@@ -0,0 +1,26 @@
+module.exports = function(mongoose) {
+
+  var wordCountSchema = new mongoose.Schema({
+    discord_id:     String,
+    reminder_time:  String,
+    // Every update, update total Sum.
+    count_total:    Number,
+    // Every Day, keep one count per day and sum it
+    count_day:    { 
+      type: Map,
+      of:   Number
+    },
+    // Every Month, aggregate count by Month
+    count_month:  { 
+      type: Map,
+      of:   Number
+    },
+    // Every Year, aggregate count by Year
+    count_year:  { 
+      type: Map,
+      of:   Number
+    }
+  });
+
+  return mongoose.model('wordCounts', wordCountSchema);
+};
